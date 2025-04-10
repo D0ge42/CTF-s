98358528bafcb2607857d3128bf3234773f98e5d
Lorenzo Nulli <lonulli@e3r7p1.42roma.it>
2025-04-10 11:55:47 +0200

    Added websecurity folder and made first ctf

---
+++ websecurity/web01/script.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+#IN this ctf we're asked to make a simple get request to retrieve the flag.
+# Once we've done the get request, we can just print it by accessing the response with .text property.
+
+import requests
+
+def main():
+    flag = requests.get("http://web-01.challs.olicyber.it/")
+    print(flag.text)
+if __name__ == "__main__":
+    main()
